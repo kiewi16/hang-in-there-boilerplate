a9f92a7c8bbad4c44d505826f528a898a0e98390
Willem Pol <wpol.edu@gmail.com>
2024-03-28 20:03:49 -0600

    ported buttons into javascript document

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ var posterImage = document.querySelector('.poster-img')
 var posterTitle = document.querySelector('.poster-title')
 var posterQuote = document.querySelector('.poster-quote') 
 var showRandomButton = document.querySelector('.show-random')
+var savePosterButton = document.querySelector('.save-poster')
+var showSavedButton = document.querySelector('.show-saved')
+var showFormButton = document.querySelector('.show-form')
+
 
 // we've provided you with some data to work with 👇
 var images = [
